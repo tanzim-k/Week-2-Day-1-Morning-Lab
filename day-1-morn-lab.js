a2aa3273e1db0d59684f6409eee8af4a506eea31
Tanzim Kazi <tanzimk96@gmail.com>
2020-11-19 13:37:41 -0500

    Morning Lab

---
+++ day-1-morn-lab.js
@@ -0,0 +1,24 @@
+let grade = 30
+
+if(grade >= 90){
+    console.log("You got an A")
+} else if (grade >= 80 && grade < 90){
+    console.log("You got a B")
+} else if (grade >= 70 && grade < 80 ){
+    console.log("You got a C") 
+} else if (grade >= 55 && grade < 70){
+    console.log("You got a D")
+} else{
+    console.log("You got a F")
+}
+
+
+let time = 18
+
+if(time >= 5 && time < 12){
+    console.log("Good Morning")
+} else if(time >= 12 && time < 16){
+    console.log("Good Afternoon")
+} else if(time >=16 && time <= 24){
+    console.log("Hey there")
+}
